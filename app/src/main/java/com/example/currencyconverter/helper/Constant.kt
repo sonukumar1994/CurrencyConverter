adc21bcb3112a4a4475bac1e25d9354d8e8c14ef
Sonu Kumar <sonu.kumar1@xebia.com>
2022-07-07 14:38:40 +0530

    API Key Changed

--- app/src/main/java/com/example/currencyconverter/helper/Constant.kt
+++ app/src/main/java/com/example/currencyconverter/helper/Constant.kt
@@ -3,7 +3,7 @@ package com.example.currencyconverter.helper
 class Constant {
     companion object{
         const val BASE_URL="https://api.apilayer.com/exchangerates_data/"
-        const val API_KEY="QwavGqaKxuhZ0DvTDZOmD3mB4RdU2kXt"
+        const val API_KEY="0mkkUUBkYVHXa1sPmlDQPW4aHArL46zO"  //"QwavGqaKxuhZ0DvTDZOmD3mB4RdU2kXt"
         const val CONVERT_URL="convert"
         const val TIME_SERIES_URL="timeseries"
         const val LATEST_URL="latest"
